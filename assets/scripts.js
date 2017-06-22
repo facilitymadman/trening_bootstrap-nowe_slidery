049acc304a754a787ce270dd7e3c4b2601dffde7
Tomi Pawlowski <pszennica@gmail.com>
2017-06-22 15:11:32 +0200

    first commit

---
+++ assets/scripts.js
@@ -0,0 +1,7 @@
+        var swiper = new Swiper('.swiper-container', {
+            pagination: '.swiper-pagination',
+            paginationClickable: true,
+            grabCursor: true,
+            loop: true,
+            controlInverse: true
+        });
